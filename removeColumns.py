0fc0e2cc2f9ff9fecec80a11fc736216008ccaef
yiran <yx1350@nyu.edu>
2017-04-13 18:56:20 -0400

    change input/output file name

--- removeColumns.py
+++ removeColumns.py
@@ -1,8 +1,8 @@
 import csv
-with open("trackData413.csv","r") as source:
+with open("trackData.csv","r") as source:
     #rdr= [x.decode('utf8').strip() for x in source.readlines()]
     rdr= csv.reader( source )
-    with open("trackData413_2.csv","w") as result:
+    with open("trackData_good.csv","w") as result:
         wtr= csv.writer( result )
         for r in rdr:
             del r[6:]
