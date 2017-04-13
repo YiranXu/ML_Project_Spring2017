5200e34329a03707972aec1421fc7b56222aa670
yiran <yx1350@nyu.edu>
2017-04-13 18:55:45 -0400

    change input/output file name

--- txtTocsv.py
+++ txtTocsv.py
@@ -4,7 +4,7 @@ with open('trackData2.txt', 'r') as in_file:
     stripped = (line.strip() for line in in_file)
     lines = (line.split("|") for line in stripped if line)
     
-    with open('trackData413.csv', 'w',encoding='utf-8') as out_file:
+    with open('trackData.csv', 'w',encoding='utf-8') as out_file:
         writer = csv.writer(out_file)
         writer.writerow(('trackID', 'AlbumId','ArtistId','genreId_1','genreId_2','genreId_3'))
         writer.writerows(lines)
