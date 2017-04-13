32c3ba0cbe3f5b231cf4923ca98410a9dbaec875
yiran <yx1350@nyu.edu>
2017-04-13 18:33:08 -0400

    convert trackData2.txt to csv

---
+++ txtTocsv.py
@@ -0,0 +1,10 @@
+import csv
+
+with open('trackData2.txt', 'r') as in_file:
+    stripped = (line.strip() for line in in_file)
+    lines = (line.split("|") for line in stripped if line)
+    
+    with open('trackData413.csv', 'w',encoding='utf-8') as out_file:
+        writer = csv.writer(out_file)
+        writer.writerow(('trackID', 'AlbumId','ArtistId','genreId_1','genreId_2','genreId_3'))
+        writer.writerows(lines)
